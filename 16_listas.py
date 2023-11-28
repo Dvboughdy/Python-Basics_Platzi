a66fe8d675cdd2bffba60389eed2698d6f8a97a2
Dvboughdy <alejoyaba@gmail.com>
2023-11-28 12:19:01 -0500

    Modificación de archivo de listas.py

--- 16_listas.py
+++ 16_listas.py
@@ -28,7 +28,8 @@ print(tasks[0])  # Imprime el primer elemento de la lista
 # Se pueden hacer ajustes o cambios que se conoce como mutación pero solo en listas en cambio en strings no se puede
 
 text = "Hola mundo"
-# text[0] = 'h'
+text[0] = 'h'
+print(text)
 # Esto no se puede hacer en strings dado que no se pueden mutar de dicha manera pero si se pueden mediante replace por otro lado en listas si se puede
 
 # Ejemplo de mutación en listas
